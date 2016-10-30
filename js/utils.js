0d59a2fb851e2f941bcade2eae6f5c34745d5605
insomniahl <195406708@qq.com>
2016-10-30 22:20:25 +0800

    lesson_4
    加速度，模拟重力，太空飞机

--- js/utils.js
+++ js/utils.js
@@ -83,6 +83,24 @@ window.utils.captureTouch = function (element) {
     return touch;
 };
 
+//转换颜色
+window.utils.parseColor = function (color, toNumber) {
+    if (toNumber === true) {
+        if (typeof color === 'number') {
+            return (color | 0); //chop off decimal
+        }
+        if (typeof color === 'string' && color[0] === '#') {
+            color = color.slice(1);
+        }
+        return window.parseInt(color, 16);
+    } else {
+        if (typeof color === 'number') {
+            color = '#' + ('00000' + (color | 0).toString(16)).substr(-6); //pad
+        }
+        return color;
+    }
+};
+
 /*
  处理动画(兼容性处理)
  requestAnimationFrame方法
