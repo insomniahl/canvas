eda14ac27382b603e36c060e1342d74f2ad381dc
insomniahl <195406708@qq.com>
2016-10-31 21:29:15 +0800

    lesson_5
    运动边界与摩擦力

--- js/ball.js
+++ js/ball.js
@@ -17,8 +17,8 @@ function Ball(radius, color){
     this.mass = 1;
     this.scaleX = 1;
     this.scaleY = 1;
-    //this.color = utils.parseColor(color);
-    this.color = color;
+    this.color = utils.parseColor(color);
+    //this.color = color;
     this.lineWidth = 1;
 }
 
