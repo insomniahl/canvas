3df2de75ba51957e943cae4540aabccbd523e730
insomniahl <195406708@qq.com>
2016-11-09 21:57:46 +0800

    lesson_9
    碰撞检测

--- js/utils.js
+++ js/utils.js
@@ -106,6 +106,11 @@ window.utils.containsPoint = function (rect, x, y) {
     return !(x < rect.x || x > rect.x + rect.width || y < rect.y || y > rect.y + rect.height);
 };
 
+
+window.utils.intersects = function (rectA, rectB) {
+    return !(rectA.x + rectA.width < rectB.x || rectB.x + rectB.width < rectA.x || rectA.y + rectA.height < rectB.y || rectB.y + rectB.height < rectA.y);
+};
+
 /*
  处理动画(兼容性处理)
  requestAnimationFrame方法
